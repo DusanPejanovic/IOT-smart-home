8c7d62a20bab46eeb9e0908cc85ac17bc63929e3
DusanPejanovic <pejanovic.dusan2001@gmail.com>
2024-01-24 15:54:49 +0100

    Add components for pi

--- smarthome-front/src/app/app.module.ts
+++ smarthome-front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Pi1Component } from './pi1/pi1.component';
+import { Pi2Component } from './pi2/pi2.component';
+import { Pi3Component } from './pi3/pi3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pi1Component,
+    Pi2Component,
+    Pi3Component
   ],
   imports: [
     BrowserModule,
